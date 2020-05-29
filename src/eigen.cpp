47b5bd45981965a6ec1d8226fe90bf27751ebe6e
ZarthaxX <tomastropea@hotmail.com>
2020-05-29 14:54:34 -0300

    completed power_iteration and get_first_eigenvalues methods

--- src/eigen.cpp
+++ src/eigen.cpp
@@ -8,23 +8,40 @@ using namespace std;
 
 pair<double, Vector> power_iteration(const Matrix& X, unsigned num_iter, double eps)
 {
-    Vector b = Vector::Random(X.cols());
-    double eigenvalue;
-    /***********************
-     * COMPLETAR CODIGO
-     **********************/
+	Vector b = Vector::Random(X.cols());
 
-    return make_pair(eigenvalue, b / b.norm());
+	//Calculate iteratively b_k+1 = X*b_k / ||X*b_k||
+	while (num_iter-- > 0) {
+		Eigen::VectorXd b_k = X * b;
+
+		double b_k_norm = b_k.norm();
+
+		b = b_k / b_k_norm;
+	}
+
+	//eigenvalue associated to this eigenvector = (vt A v) / vt v
+	double eigval = b.transpose() * X * b;
+	eigval = eigval / (b.transpose() * b);
+	return make_pair(eigval, b / b.norm());
 }
 
 pair<Vector, Matrix> get_first_eigenvalues(const Matrix& X, unsigned num, unsigned num_iter, double epsilon)
 {
-    Matrix A(X);
-    Vector eigvalues(num);
-    Matrix eigvectors(A.rows(), num);
+	Matrix A(X);
+	Vector eigvalues(num);
+	Matrix eigvectors(A.rows(), num);
 
-    /***********************
-     * COMPLETAR CODIGO
-     **********************/
-    return make_pair(eigvalues, eigvectors);
+	for (int i = 0; i < X.cols(); i++) {
+		//Find the eigenvector and the associated eigenvalue
+		auto eigen_pair = power_iteration(A, num_iter, 0.0f);
+		//Assign the ith eigen value to the ith coordinate
+		eigvalues(i) = eigen_pair.first;
+		//Assign the ith eigen vector to the ith column 
+		eigvectors.block(0, i, eigvectors.rows(), 1) = eigen_pair.second;
+
+		//Subatract to matrix A, A - lambda * v * vt
+		A -= eigen_pair.first * (eigen_pair.second * eigen_pair.second.transpose());
+	}
+
+	return make_pair(eigvalues, eigvectors);
 }
