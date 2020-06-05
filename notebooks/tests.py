2d31ed14d2102487f5c0de85ce2283c1ebedf8c3
Manuel Panichelli <panicmanu@gmail.com>
2020-06-05 20:16:14 -0300

    knn: tests

---
+++ notebooks/tests.py
@@ -0,0 +1,66 @@
+"""Corre los tests"""
+
+import metnum
+import unittest
+
+import numpy as np
+import scipy
+
+class TestKNNClassifier(unittest.TestCase):
+    def test_same(self):
+        """Testea que predecir la matriz de entrenamiento de el mismo res"""
+        classifier = metnum.KNNClassifier(1)
+
+        X_train = np.array([
+            [1, 2, 3],
+            [6, 6, 6],
+            [0, 1, 0],
+        ])
+
+        y_train = np.array([
+            [0],
+            [2],
+            [3],
+        ])
+
+        classifier.fit(X_train, y_train)
+        
+        y = classifier.predict(X_train)
+
+        self.assertTrue(np.allclose(y.reshape(3, 1), y_train))
+    
+    def test_little(self):
+        """Testea un caso en R2"""
+        classifier = metnum.KNNClassifier(3)
+
+        X_train = np.array([
+            [0, 0],   [1, 0],   [0, 1],   [1, 1],
+            [5, 5],   [6, 5],   [5, 6],   [6, 6],
+            [20, 20], [21, 20], [20, 22], [22, 20],
+        ])
+
+        y_train = np.array([
+            [0], [0], [0], [0],
+            [5], [5], [5], [5],
+            [9], [9], [9], [9],
+        ])
+
+        classifier.fit(X_train, y_train)
+
+        X = np.array([
+            [15, 15], # 9
+            [10, 10], # 5
+            [2, 2],   # 0
+        ])
+
+        want = np.array([9.0, 5.0, 0.0])
+        got = classifier.predict(X)
+
+        assertAllClose(self, want, got)
+
+def assertAllClose(self, want, got):
+    """Se fija que dos vectores esten np.allclose"""
+    self.assertTrue(np.allclose(want, got), f"want: {want}, but got: {got}")
+
+if __name__ == '__main__':
+    unittest.main()
