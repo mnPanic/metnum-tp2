f60006ff271a08b565407266171b1ecb402b7053
Manuel Panichelli <panicmanu@gmail.com>
2020-06-05 01:29:21 -0300

    knn: Implementacion inicial

--- src/knn.cpp
+++ src/knn.cpp
@@ -6,24 +6,58 @@
 using namespace std;
 
 
-KNNClassifier::KNNClassifier(unsigned int n_neighbors)
-{
+KNNClassifier::KNNClassifier(unsigned int n_neighbors): k_neighbors(n_neighbors) {
 }
 
-void KNNClassifier::fit(Matrix X, Matrix y)
-{
+void KNNClassifier::fit(Matrix X, Matrix y) {
+    X_train = X;
+    y_train = y;
 }
 
-
-Vector KNNClassifier::predict(Matrix X)
-{
+Vector KNNClassifier::predict(Matrix X) {
     // Creamos vector columna a devolver
-    auto ret = Vector(X.rows());
+    auto classifications = Vector(X.rows());
 
-    for (unsigned k = 0; k < X.rows(); ++k)
-    {
-        ret(k) = 0;
+    for (unsigned k = 0; k < X.rows(); ++k) {
+        // X.row(k) es una imagen que queremos clasificar
+        Vector img = X.row(k);
+
+        // Nos quedamos con los K vecinos mas cercanos para cada imagen
+        // k vecinos mas cercanos ordenados por distancia de forma ascendente
+        OrderedArray arr(this->k_neighbors);
+
+        // Vemos la distancia a cada una de las imagenes del training set
+        for(unsigned i = 0; i < X_train.rows(); ++i) {
+            Vector img_train = X_train.row(k);
+
+            // Nos basta con tomar la norma al cuadrado, pues
+            // la relacion de orden se mantiene.
+            double dist = (img - img_train).transpose() * (img - img_train);
+
+            // Insertamos de forma ordenada en k_nearest
+            arr.insert(neighbor{dist, y_train(k, 0)});
+        }
+
+        // En arr.k_neighbors tenemos los k vecinos mas cercanos.
+        // Buscamos la moda
+        int votes[10];
+        for (neighbor n : arr.k_nearest) {
+            // xlas
+            assert(0 <= n.digit && n.digit < 10);
+            votes[n.digit]++;
+        }
+
+        int max = -1;
+        int classification = -1;
+        for(int i = 0; i < 10; i++) {
+            if (votes[i] > max) { 
+                max = votes[i];
+                classification = i;
+            }
+        }
+
+        classifications(k) = classification;
     }
 
-    return ret;
+    return classifications;
 }
--- src/knn.h
+++ src/knn.h
@@ -1,7 +1,8 @@
 #pragma once
 
 #include "types.h"
-
+#include <vector>
+#include <iostream>
 
 class KNNClassifier {
 public:
@@ -10,5 +11,63 @@ public:
     void fit(Matrix X, Matrix y);
 
     Vector predict(Matrix X);
+
 private:
+
+    // matriz de entrenamiento con imagenes
+    Matrix X_train;
+
+    // clasificaciones
+    Matrix y_train;
+
+    // Cantidad de vecinos a considerar
+    int k_neighbors;
+};
+
+struct neighbor {
+    double dist;
+    int digit;
+};
+
+// para tests
+std::ostream& operator<<(std::ostream& os, const neighbor n) { 
+    os << "[" <<  n.digit << "] " << n.dist;
+    return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const std::vector<neighbor> ns) {
+    for(neighbor n : ns) {
+        os << ns << "; ";
+    }
+
+    return os;
+}
+
+struct OrderedArray {
+    // ordenado en forma ascendente
+    std::vector<neighbor> k_nearest;
+    OrderedArray(int k): k_nearest(k, neighbor{INFINITY, 0}) {}
+
+    // Inserta manteniendo el orden, si hace overflow del contenedor
+    // descarta el elemento.
+    void insert(neighbor n) {
+        int k = k_nearest.size();
+
+        if(n.dist >= k_nearest[k - 1].dist) {
+            // No nos interesa
+            return;
+        }
+
+        // Si es menor al ultimo (el mas grande), lo swapeamos y luego
+        // lo llevamos al lugar donde pertenece swapeando.
+        k_nearest[k - 1] = n;
+
+        for (
+            int i = k - 1; 
+            i > 0 && k_nearest[i].dist < k_nearest[i - 1].dist;
+            i--
+        ) {
+            swap(k_nearest[i], k_nearest[i - 1]);
+        }
+    }
 };
