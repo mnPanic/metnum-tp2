a6d4d0168d2c737b2246adb41cf2675e5095cd65
Manuel Panichelli <panicmanu@gmail.com>
2020-06-12 01:08:27 -0300

    Ejecutable y kaggle submission

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # metnum-tp2
+
 TP2 de Metodos Numéricos de Computación, exactas.
+
+## Kaggle
+
+Para realizar el submission para kaggle,
+
+1. Compilar
+
+   ```bash
+   ./build
+   ```
+
+2. Correr el ejecutable con los datos de test y train
+
+    ```bash
+    cd build
+    $ ./tp2 -m 1 -i ../data/train.csv -q ../data/test.csv -o ../data/kaggle_submission_kNN_6_pow_5_PCA_34.csv
+    reading matrix & labels from file: ../data/train.csv
+    reading matrix from file: ../data/test.csv
+    Running kNN+PCA with k=6 distance weights & alpha=34
+    writing vector to csv file: kaggle_submission_kNN_6_pow_5_PCA_34.csv
+    done!
+    ```
+
+3. Done! Ahora se puede submittear a [kaggle](https://www.kaggle.com/c/digit-recognizer/submit).
--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,158 @@
 //
 
 #include <iostream>
+#include <string>
+#include <fstream>
+
 #include "pca.h"
 #include "eigen.h"
+#include "knn.h"
+
+struct arguments {
+    int method;
+    std::string train_set;
+    std::string test_set;
+    std::string classif;
+};
+
+// Parsea los argumentos exitieando si no son del formato correcto.
+arguments parse_argv(int argc, char** argv) {
+    const int ARG_COUNT = 9;
+    // $ ./tp2 -m <method> -i <train_set> -q <test_set> -o <classif>
+    if (argc != ARG_COUNT) {
+        std::cout
+            << "Wrong number of args. Usage:\n\t"
+            << "$ ./tp2 -m <method> -i <train_set> -q <test_set> -o <classif>"
+            << std::endl;
+        exit(1);
+    }
+
+    arguments args;
+
+    for(int i = 1; i < ARG_COUNT; i+=2) {
+        // i   es el arg
+        // i+1 el val
+        std::string arg = argv[i];
+        std::string val = argv[i+1];
+
+        // Como puede ser que c++ no te deje hacer switch en string?
+        if(arg == "-m") args.method = atoi(val.c_str());
+        if(arg == "-i") args.train_set = val;
+        if(arg == "-q") args.test_set = val;
+        if(arg == "-o") args.classif = val;
+    }
+
+    return args;
+}
+
+void vec2kagglecsv(Vector v, const std::string& path) {
+    std::cout << "writing vector to csv file: " << path << std::endl;
+    std::ofstream file(path);
+    file << "ImageId,Label" << std::endl;
+    for(int i = 0; i < v.rows(); i++) {
+        file << i + 1 << "," << v.row(i) << std::endl;
+    }
+
+    std::cout << "done!" << std::endl;
+}
+
+Matrix csv2mat(const std::string& path) {
+    std::cout << "reading matrix from file: " << path << std::endl;
+    std::ifstream indata;
+    indata.open(path);
+
+    std::string line;
+    std::vector<double> values;
+    uint rows = 0;
+    // ignore the first line
+    std::getline(indata, line);
+    while (std::getline(indata, line)) {
+        std::stringstream lineStream(line);
+        std::string cell;
+
+        while (std::getline(lineStream, cell, ',')) {
+            values.push_back(std::stod(cell));
+        }
+
+        ++rows;
+    }
+
+    return Eigen::Map<Matrix>(values.data(), rows, values.size()/rows);
+}
+
+std::tuple<Matrix, Matrix> csv2matlabel(const std::string& path) {
+    std::cout << "reading matrix & labels from file: " << path << std::endl;
+    std::ifstream indata;
+    indata.open(path);
+
+    std::string line;
+    std::vector<double> values;
+    std::vector<double> labels;
+
+    uint rows = 0;
+
+    // ignore the first line
+    std::getline(indata, line);
+
+    while (std::getline(indata, line)) {
+        std::stringstream lineStream(line);
+        std::string cell;
+
+        // The first element is its label
+        std::getline(lineStream, cell, ',');
+        labels.push_back(std::stod(cell));
+
+        while (std::getline(lineStream, cell, ',')) {
+            values.push_back(std::stod(cell));
+        }
+
+        ++rows;
+    }
+
+    return std::tuple<Matrix, Matrix>(
+        Eigen::Map<Matrix>(values.data(), rows, values.size()/rows),
+        Eigen::Map<Matrix>(labels.data(), rows, 1)
+    );
+}
+
+constexpr int METHOD_KNN = 0;
+constexpr int METHOD_KNN_PCA = 1;
 
 int main(int argc, char** argv){
+    arguments args = parse_argv(argc, argv);
 
-    Matrix m;
-    PCA pca(1);
-    std::cout << "===== Random: ======" << std::endl;
-    m = Eigen::DiagonalMatrix<double, 3>(3, 8, 6);
-    std::cout << "Resultado:\n " << pca.transform(m) << std::endl;
+    std::tuple<Matrix, Matrix> trainpair = csv2matlabel(args.train_set);
+    Matrix train = std::get<0>(trainpair);
+    Matrix labels = std::get<1>(trainpair);
+    Matrix test = csv2mat(args.test_set);
 
+    Vector pred;
+    switch(args.method) {
+        case METHOD_KNN:
+        {
+            std::cout << "Running kNN with k=4 and distance weights" << std::endl;
+
+            KNNClassifier clf(4, "distance_pow");
+            clf.fit(train, labels);
+            pred = clf.predict(test);
+            break;
+        }
+        case METHOD_KNN_PCA:
+        {
+            std::cout << "Running kNN+PCA with k=6 distance weights & alpha=34" << std::endl;
+            KNNClassifier clf(6, "distance_pow_5");
+            PCA pca(34);
+
+            pca.fit(train);
+            clf.fit(pca.transform(train), labels);
+            pred = clf.predict(pca.transform(test));
+            break;
+        }
+        default:
+            std::cout << "Invalid method. 0: kNN y 1: kNN+PCA." << std::endl;
+            exit(1);
+    }
+
+    vec2kagglecsv(pred, args.classif);
     return 0;
 }
